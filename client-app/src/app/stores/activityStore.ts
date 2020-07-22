9c687b5b8abe9976e07ef00e86419993d29099cb
Binh Nguyen <bnguyen@dwsubsea.com>
2020-07-22 10:15:56 -0500

    Re-Styling

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -13,9 +13,20 @@ class ActivityStore {
   @observable target = '';
 
   @computed get activitiesByDate() {
-    return Array.from(this.activityRegisty.values()).sort(
+    return this.groupActivitiesByDate(
+      Array.from(this.activityRegisty.values())
+    );
+  }
+
+  groupActivitiesByDate(activities: IActivity[]) {
+    const sortedActivities = activities.sort(
       (a, b) => Date.parse(a.date) - Date.parse(b.date)
     );
+    return Object.entries(sortedActivities.reduce((activities, activity)=>{
+      const date = activity.date.split('T')[0];
+      activities[date] = activities[date]?[...activities[date], activity] : [activity];
+      return activities;
+    }, {} as {[key: string]: IActivity[]}));
   }
 
   @action loadActivities = async () => {

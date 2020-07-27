ea16981192e50d16f521756b793930aacc52e693
Binh Nguyen <bnguyen@dwsubsea.com>
2020-07-27 14:33:09 -0500

    Adding Client side attendances

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -67,6 +67,8 @@ const Activities = {
   update: (activity: IActivity) =>
     requests.put(`/activities/${activity.id}`, activity),
   delete: (id: string) => requests.delete(`/activities/${id}`),
+  attend: (id: string) => requests.post(`/activities/${id}/attend`, {}),
+  unattend: (id: string) => requests.delete(`/activities/${id}/attend`),
 };
 
 const User = {
--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -1,3 +1,4 @@
+import { setAtivityProps, createAttendee } from './../common/util/util';
 import { RootStore } from './rootStore';
 import { IActivity } from './../models/activity';
 import { observable, action, computed, runInAction } from 'mobx';
@@ -17,6 +18,7 @@ export default class ActivityStore {
   @observable loadingInitial = false;
   @observable submitting = false;
   @observable target = '';
+  @observable loading = false;
 
   @computed get activitiesByDate() {
     return this.groupActivitiesByDate(
@@ -45,7 +47,7 @@ export default class ActivityStore {
       const activities = await agent.Activities.list();
       runInAction('loading activities', () => {
         activities.forEach((activity) => {
-          activity.date = new Date(activity.date);
+          setAtivityProps(activity, this.rootStore.userStore.user!);
           this.activityRegisty.set(activity.id, activity);
         });
         this.loadingInitial = false;
@@ -69,7 +71,7 @@ export default class ActivityStore {
       try {
         activity = await agent.Activities.details(id);
         runInAction('getting activity', () => {
-          activity.date = new Date(activity.date);
+          setAtivityProps(activity, this.rootStore.userStore.user!);
           this.activity = activity;
           this.activityRegisty.set(activity.id, activity);
           this.loadingInitial = false;
@@ -96,6 +98,12 @@ export default class ActivityStore {
     this.submitting = true;
     try {
       await agent.Activities.create(activity);
+      const attendee = createAttendee(this.rootStore.userStore.user!);
+      attendee.isHost = true;
+      let attendees = [];
+      attendees.push(attendee);
+      activity.attendees = attendees;
+      activity.isHost = true;
       runInAction('creating activity', () => {
         this.activityRegisty.set(activity.id, activity);
         this.submitting = false;
@@ -149,6 +157,49 @@ export default class ActivityStore {
       });
 
       console.log(error);
+    }
+  };
+
+  @action attendActivity = async () => {
+    const attendee = createAttendee(this.rootStore.userStore.user!);
+    this.loading = true;
+    try {
+      await agent.Activities.attend(this.activity!.id);
+      runInAction(() => {
+        if (this.activity) {
+          this.activity.attendees.push(attendee);
+          this.activity.isGoing = true;
+          this.activityRegisty.set(this.activity?.id, this.activity);
+          this.loading = false;
+        }
+      });
+    } catch (error) {
+      runInAction(() => {
+        this.loading = false;
+      });
+      toast.error('Problem signing up to activity');
+    }
+  };
+
+  @action cancelAttendance = async () => {
+    this.loading = true;
+    try {
+      await agent.Activities.unattend(this.activity!.id);
+      runInAction(() => {
+        if (this.activity) {
+          this.activity.attendees = this.activity.attendees.filter(
+            (a) => a.username !== this.rootStore.userStore.user!.username
+          );
+          this.activity.isGoing = false;
+          this.activityRegisty.set(this.activity.id, this.activity);
+          this.loading = false;
+        }
+      });
+    } catch (error) {
+      runInAction(() => {
+        this.loading = false;
+      });
+      toast.error('Problem cancelling attendance');
     }
   };
 }
